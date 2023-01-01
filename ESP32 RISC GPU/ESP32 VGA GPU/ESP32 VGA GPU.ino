5410063946ef38ceb85c8a454a7af2241f5b75af
YuzuMin <69055192+YuzuMin@users.noreply.github.com>
2023-01-02 00:54:45 +0800

    Update ESP32 VGA GPU.ino

--- ESP32 RISC GPU/ESP32 VGA GPU/ESP32 VGA GPU.ino
+++ ESP32 RISC GPU/ESP32 VGA GPU/ESP32 VGA GPU.ino
@@ -14,12 +14,42 @@ const int hsyncPin = 32;
 const int vsyncPin = 33;
 
 //ASCII Char Values
-const char NUL = 0x00;
-const char SOH = 0x01;
-const char STX = 0x02;
-const char ETX = 0x03;
-const char ENQ = 0x04;
-const char  LF = 0x10;
+const char NUL = 0x00;  //null
+const char SOH = 0x01;  //start of header
+const char STX = 0x02;  //start of text
+const char ETX = 0x03;  //end of text
+const char EOT = 0x04;  //end of transmission
+const char ENQ = 0x05;  //enquiry
+const char ACK = 0x06;  //acknowledge
+const char BEL = 0x07; 	//bell (ring)
+const char BS  = 0x08; 	//backspace
+const char HT  = 0x09; 	//horizontal tab
+const char LF  = 0x0A;  //line feed
+const char VT  = 0x0B;	//vertical tab
+const char FF  = 0x0C; 	//form feed
+const char CR  = 0x0D; 	//carriage return
+const char SO  = 0x0E; 	//shift out
+const char SI  = 0x0F; 	//shift in
+const char DLE = 0x10;  //data link escape
+const char DC1 = 0x11;	//device control 1
+const char DC2 = 0x12;	//device control 2
+const char DC3 = 0x13;	//device control 3
+const char DC4 = 0x14;	//device control 4
+const char NAK = 0x15;	//negative acknowledge
+const char SYN = 0x16;	//synchronize
+const char ETB = 0x17;	//end transmission block
+const char CAN = 0x18;	//cancel
+const char EM  = 0x19;	//end of medium
+const char SUB = 0x1A;	//substitute
+const char ESC = 0x1B;	//escape
+const char FS  = 0x1C;	//file separator
+const char GS  = 0x1D;	//group separator
+const char RS  = 0x1E;	//record separator
+const char US  = 0x1F;	//unit separator
+const char DEL = 0x7F;	//delete (rubout)
+
+//Custom Char Values
+const char RSFH = 0x80;	//Refresh
 
 
 //VGA Device
@@ -47,7 +77,8 @@ void loop()
 
 
     switch (input) {
-      case NUL:
+      case RSFH:
+        //Clears Screen and Reset Cursor
         vga.setCursor(0,0);
         vga.clear(0);
         break;
