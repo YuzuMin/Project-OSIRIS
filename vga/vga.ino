943f583aa3fd4e55ca9904ec493c494758b8d63c
YuzuMin <69055192+YuzuMin@users.noreply.github.com>
2023-01-01 19:59:43 +0800

    Update vga.ino

--- vga/vga.ino
+++ vga/vga.ino
@@ -13,6 +13,14 @@ const int bluePin = 12;
 const int hsyncPin = 32;
 const int vsyncPin = 33;
 
+//ASCII Char Values
+const char NUL = 0x00;
+const char SOH = 0x01;
+const char STX = 0x02;
+const char ETX = 0x03;
+const char ENQ = 0x04;
+
+
 //VGA Device
 VGA3Bit vga;
 
@@ -25,6 +33,7 @@ void setup()
 	vga.setFont(Font6x8);
 	//displaying the text
 	vga.println("Hello World!");
+  vga.println("ESP32 VGA GPU");
   //initialize serial to get input data
   Serial.begin(921600);
 }
@@ -34,13 +43,29 @@ void loop()
   if(Serial.available()){
     //read serial input
     char input = Serial.read();
-    vga.print(input);
 
-    
-    if(input==' '){
-      Serial.println("space");
+    if(input==0x00){
+      vga.setCursor(0,0);
+      vga.clear(0);
+    }else{
+      vga.print(input);
+    }
+
+
+
+    //Code for String inputs (Note: Very slow)
+    /*
+    String strinput = Serial.readString();
+  
+    if(strinput == "clear"){
       vga.setCursor(0,0);
       vga.clear(0);
     }
+    if(strinput == "\n"){
+      vga.println("");
+    }
+
+    vga.print(strinput.c_str());
+    */
   }
 }
